0a3b61c0510b098503ea50ccdb48ac0c09591002
pcannon09 <pcannon775@gmail.com>
2025-02-20 21:19:19 +0000

    All your info about date, timezone and time is here

---
+++ test/src/time.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <unistd.h>
+#include <chrono>
+#include <thread>
+
+#include "../../inc/Time.hpp"
+
+namespace tests
+{
+    int time()
+    {
+        stt::Time time("main");
+
+        stt::TimeZoneInfo tzInfo;
+        stt::DateInfo dateInfo;
+        stt::TimeInfo timeInfo;
+
+        while (1)
+        {
+            time.update(tzInfo);
+            time.update(dateInfo);
+            time.update(timeInfo);
+
+            timeInfo.is12hFormat = time.set12hFormat(true);
+
+            std::cout
+                << "- TIME ZONE INFO -\n"
+                << "GMT Off? : " << tzInfo.gmtOff << "\n"
+                << "Is DST? : " << tzInfo.isDST << "\n"
+                << "Current timezone : " << tzInfo.timezone << "\n"
+                << "Current UTC timezone : " << tzInfo.utcTimezone << "\n"
+
+                << "- TIME INFO -\n"
+                << "Hour: " << timeInfo.hour <<  "\n"
+                << "Min : " << timeInfo.min << "\n"
+                << "Sec : " << timeInfo.sec << "\n"
+                << "MS : " << timeInfo.ms << "\n"
+                << "Is AM? : " << timeInfo.isAm << "\n"
+                << "Is 12 hour format? : " << timeInfo.is12hFormat << "\n"
+
+                << "- DATE INFO -\n"
+                << "Year : " << dateInfo.year << "\n"
+                << "Month : " << dateInfo.month << "\n"
+                << "Day : " << dateInfo.day << "\n"
+                << "Week day : " << dateInfo.weekDay << "\n"
+                << "Year day : " << dateInfo.yearDay << "\n"
+
+                << "\r" << std::flush;
+
+                std::this_thread::sleep_for(std::chrono::milliseconds(10));
+
+                system("clear");
+        }
+
+        return 0;
+    }
+}
