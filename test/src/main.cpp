02c715425dfce82ec7b01d51ad7737a511972324
pcannon09 <pcannon775@gmail.com>
2025-02-20 17:22:10 +0000

    Positional params and added timer tests

--- test/src/main.cpp
+++ test/src/main.cpp
@@ -2,13 +2,19 @@
 
 #include "../../test/inc/tests.hpp"
 
-int main()
+int main(int argc, char *argv[])
 {
     std::string answer;
 
-    std::cout << "What test do you want to run? Timer (timer), time (time) or stopwatch (stopwatch)\n>>> ";
-    std::cin >> answer;
-    
+    if (argc > 1)
+    { answer = argv[1]; }
+
+    else
+    {
+        std::cout << "What test do you want to run? Timer (timer), time (time) or stopwatch (stopwatch)\n>>> ";
+        std::cin >> answer;
+    }
+
     if (answer == "stopwatch")
     {
         std::cout << "Starting **Stopwatch** test\n";
@@ -18,7 +24,9 @@ int main()
 
     else if (answer == "timer")
     {
+        std::cout << "Starting **Timer** test\n";
 
+        return tests::timer();
     }
 
     else if (answer == "time")
