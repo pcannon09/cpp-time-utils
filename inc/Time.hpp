6b7fe345bdc62524f96c0940a30951b45e6d384b
pcannon09 <pcannon775@gmail.com>
2025-02-21 07:58:24 +0000

    Documented the code

--- inc/Time.hpp
+++ inc/Time.hpp
@@ -37,16 +37,41 @@ namespace stt
         static unsigned int utcMinOffset;
 
     public:
+        /// Time - Constructor
+        /// Set the ID of the `Time` object
+        /// @param ID
         Time(std::string ID);
 
+        /// Set UTC offset - Function
+        /// Set the offset of UTC according to your current timezone
+        /// @param hour
+        /// @param min
         void setUTCOffset(int hour, unsigned int min);
 
+        /// Update - Function
+        /// Set the Time information and return if it was a success (0) or not (-1)
+        /// @param info
         int update(TimeInfo &info);
+
+        /// Update - Function - Overload
+        /// Set the Timezone information and return if it was a success (0) or not (-1)
+        /// @param info
         int update(TimeZoneInfo &info);
+
+        /// Update - Function - Overload
+        /// Set the Date information and return if it was a success (0) or not (-1)
+        /// @param info
+        /// @return int
         int update(DateInfo &info);
 
+        /// Set 12 Hour Format - Function
+        /// Set if it should use the 12 hour format
+        /// @return bool
         bool set12hFormat(bool set12hFormat);
 
+        /// Get ID - Function
+        /// Get the ID for the `Time` object
+        /// @return std::string
         std::string getID();
     };
 }
