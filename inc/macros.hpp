d3af4929764673a12d56f664f17f9da9f33203f6
pcannon09 <pcannon775@gmail.com>
2025-02-21 11:30:42 +0000

    Update version to 1.0.0

--- inc/macros.hpp
+++ inc/macros.hpp
@@ -3,16 +3,16 @@
 #include "macros.hpp"
 
 // Versioning
-#define STT_VERSION_MAJOR          0
+#define STT_VERSION_MAJOR          1
 #define STT_VERSION_MINOR          0
-#define STT_VERSION_PATCH          1
+#define STT_VERSION_PATCH          0
 
 // Version states:
 // dev - develoment (Before Beta version)
 // beta - Beta (Testing)
 // snap - Snapshot (Future update)
 // build - Built version
-#define STT_VERSION_STATE          "beta"
+#define STT_VERSION_STATE          "build"
 
 #define STT_VERSION                ((STT_VERSION_MAJOR<<16)|(STT_VERSION_MINOR<<8)|(STT_VERSION_PATCH)|(STT_VERSION_STATE << 24))
 
