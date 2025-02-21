4976372671d9d604e3240976c412a814fb398b77
pcannon09 <pcannon775@gmail.com>
2025-02-21 10:29:58 +0000

    Documented code

--- inc/Sleep.hpp
+++ inc/Sleep.hpp
@@ -12,15 +12,44 @@ namespace timeUtils
         static std::string id;
 
     public:
+        /// Sleep - Constructor
+        /// @brief Create the `Sleep` object
+        /// @param ID
         Sleep(std::string ID);
 
+        /// Hours - Function
+        /// @brief Sleep for (x) hours
+        /// @param duration
         void hours(unsigned int duration);
+
+        /// Hours - Function
+        /// @brief Sleep for (x) minutes
+        /// @param duration
         void minutes(unsigned int duration);
+
+        /// Hours - Function
+        /// @brief Sleep for (x) seconds
+        /// @param duration
         void seconds(unsigned int duration);
+
+        /// Hours - Function - Overload
+        /// @brief Sleep for (x) decimal seconds
+        /// @param duration
         void seconds(double duration);
+
+        /// Hours - Function
+        /// @brief Sleep for (x) milliseconds
+        /// @param duration
         void milliseconds(unsigned int duration);
+
+        /// Hours - Function
+        /// @brief Sleep for (x) nanoseconds
+        /// @param duration
         void nanoseconds(unsigned int duration);
 
+        /// Get ID - Function
+        /// Get the ID from this object
+        /// @return std::string
         std::string getID();
     };
 }
